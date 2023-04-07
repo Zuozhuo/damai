f22dfb41c6f4dd2b26413793a6142663e31b2518
Zuozhuo <116483151+Zuozhuo@users.noreply.github.com>
2023-04-07 20:10:11 +0800

    Update 大麦.py

--- 大麦.py
+++ 大麦.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import datetime
 import traceback
 
 
@@ -70,10 +71,10 @@ def page_1(driver):
             while True:
                 WebDriverWait(driver, 10, 0.001).until(EC.text_to_be_present_in_element_attribute((By.XPATH, '//div[@class="detail-button"]/div'), 'class', 'button button-primary'))        
                 buy_btn = driver.find_element(By.XPATH, '//p[@class="buy__button__text"]')
-                print(buy_btn.text)
+                print(buy_btn.text, end='\r')
                 if '立即' in buy_btn.text:
                     break
-                elif '即将' in buy_btn.text:
+                elif '即将' in buy_btn.text or '缺货登记' in buy_btn.text:
                     driver.refresh()
             # buy_btn = WebDriverWait(driver, 10*60, 0.001).until(EC.text_to_be_present_in_element((By.XPATH, '//p[@class="buy__button__text"]'), '立即预订'))
             
@@ -96,7 +97,6 @@ def page_1(driver):
             # 确定
             buybtn = driver.find_element(By.XPATH, '//div[@class="sku-footer-bottom"]/div[text()="确定"]')
             driver.execute_script("arguments[0].click();", buybtn)
-            print('-------page_1----------')
             break
 
 def choose_viewer(driver, viewer: list[str]):
@@ -141,7 +141,7 @@ def handle_slider_captcha(driver):
     # 释放滑块
     action_chains.release().perform()
 
-def handle_iframe(driver):
+def handle_iframe(driver, mode):
     """处理网络拥堵/滑动验证"""
     try:
         div_baxia_dialog_auto = driver.find_element(By.XPATH, '//div[contains(@class, "baxia-dialog auto")]')
@@ -159,10 +159,14 @@ def handle_iframe(driver):
                 # cancel = driver.find_element(By.XPATH, '//div[@class="baxia-dialog-close" and text()="X"]')
                 # cancel.click()
                 # print('点击了×按钮')
-                print('网络拥堵，刷新网页')
-                driver.refresh()
-                return 'refresh'
-                # driver.get(page_1_url)
+                if mode == 0:
+                    print('网络拥堵，刷新网页')
+                    driver.refresh()
+                    return 'refresh'
+                elif mode == 1:
+                    print('网络拥堵，返回到选票页面')
+                    driver.get(page_1_url)
+                    return 'back'
             else:
                 print('其他？')
             driver.switch_to.default_content()
@@ -170,6 +174,19 @@ def handle_iframe(driver):
 def page_2(driver):
     """提交订单页面，主要是选择观演人,
     https://m.damai.cn/app/dmfe/h5-ultron-buy/index.html?buyParam=..."""
+    global is_direct_page_2
+    if is_direct_page_2 == 1:
+        # 计算当前时间和开票时间之间的时间差
+        while True:
+            now_time = datetime.datetime.now()
+            delta = ticket_time - now_time
+            print(delta, end='\r')
+            if delta.total_seconds() <= interval_time:
+                print('刷新')
+                driver.refresh()
+                is_direct_page_2 = 0 # 只运行一次
+                break
+
     app = WebDriverWait(driver, 2, 0.001).until(EC.presence_of_element_located((By.ID, 'app')))
     try:
         div_refresh = driver.find_element(By.XPATH, '//div[@id="app"]//div[text()="刷新"]')
@@ -183,8 +200,8 @@ def page_2(driver):
 
     handle_div_confirm(driver)
 
-    if handle_iframe(driver) == 'refresh': # 如果返回refresh，代表刷新了页面
-        return
+    if handle_iframe(driver, mode) in ('refresh', 'back'): # 如果返回refresh 或 back，代表刷新了页面
+        return 'continue'
 
     choose_viewer(driver, viewer.split('|'))
 
@@ -199,6 +216,23 @@ def page_2(driver):
             # driver.execute_script("arguments[0].click();", btn)
             actions = ActionChains(driver)
             actions.move_to_element(btn).click().perform()
+
+def priority_purchase_qualification_redemption_page(driver):
+    """优先购资格兑换页
+    https://m.taopiaopiao.com/tickets/vip/pages/rewards-detail/index.html..."""
+    while True:
+        WebDriverWait(driver, 2, 0.001).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="bui-card"]')))
+        btn = driver.find_element(By.XPATH, '//div[@class="bt"]//div[contains(@class, "bt-left")]')
+        print(btn.text)
+        if 'disable' in btn.get_attribute('class'):
+            driver.refresh()
+        else:
+            driver.execute_script("arguments[0].click();", btn)
+            break
+
+    btn_2 = WebDriverWait(driver, 2, 0.001).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="btn js-exchange-bt"]')))
+    driver.execute_script("arguments[0].click();", btn_2)
+
 
 def main():
     url = ""
@@ -220,16 +254,32 @@ def main():
             elif 'mclient.alipay.com' in url:
                 print('恭喜你！抢到票了')
                 break
+            elif 'm.taopiaopiao.com/tickets/vip/pages/rewards-detail/index.html' in url:
+                priority_purchase_qualification_redemption_page(driver)
         except Exception:
             print('出错了', traceback.format_exc())
             continue
 
 
 if __name__ == '__main__':
-    viewer = input('观演人(多个观演人用|分隔)：')
-    date = input('日期(yyyy-mm-dd)：')
-    prices = input('价格(多个价格用|分隔，越靠前表示优先级越高)：')
-    num = input('数量（须和观演人的数量一致）：')
+    viewer = input('观演人(多个观演人用|分隔，和票的数量一致)：') # 观演人
+
+    is_direct_page_2 = int(input('是否直接进入订单提交页面(是：1，否：0)：'))
+    if is_direct_page_2 == 1: # 直接选好价格数量进入page_2
+        mode = 0
+
+        ticket_time_str = input('请输入开票时间(格式为 yyyy-mm-dd HH:MM:SS 如2023-04-07 19:25:00)：')
+        # ticket_time_str = '2023-04-07 19:25:00'
+        ticket_time = datetime.datetime.strptime(ticket_time_str, '%Y-%m-%d %H:%M:%S')
+        
+        interval_time = int(input('提前几秒刷新？(输入阿拉伯数字)：'))
+    else: # 从page_1开始选
+        date = input('日期(yyyy-mm-dd)：')
+        prices = input('价格(多个价格用|分隔，越靠前表示优先级越高)：')
+        num = input('数量（须和观演人的数量一致）：')
+
+        mode = int(input('网络拥堵策略 刷新则输入0 返回到上一页面则输入1：'))
+        # mode = 0
 
     page_1_url = ''
 
